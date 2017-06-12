94c4e8c4e18c00ca51520a3fb74a2a12bfd25798
Summer <summer@ekohe.com>
2017-06-12 19:08:45 +0800

    MISC - update framework

--- LocationFramework/LocationFramework.swift
+++ LocationFramework/LocationFramework.swift
@@ -2,44 +2,65 @@
 //  LocationFramework.swift
 //  LocationFramework
 //
-//  Created by liuhaoyang on 2017/5/16.
-//  Copyright © 2017年 liuhaoyang. All rights reserved.
+//  Created by Summer on 17/05/2017.
+//  Copyright © 2017 Summer. All rights reserved.
 //
 
 import Foundation
 import UIKit
 import CoreLocation
 
-public class LocationFramework: NSObject, CLLocationManagerDelegate {
-    var locationManager = CLLocationManager()
+public class LocationFramework: NSObject, CLLocationManagerDelegate{
+    let locationManager = CLLocationManager()
+    var lastLocationUpdate: NSDate?
+    var locationInformations: [String]?
+    var updateLocation: (([String]) -> Void)?
     
     public override init() {
         super.init()
         locationManager.delegate = self
+
+    }
+    
+    public func setupLocationManager() {
+        
+        locationManager.distanceFilter = kCLHeadingFilterNone
+        locationManager.allowsBackgroundLocationUpdates = true
+        locationManager.pausesLocationUpdatesAutomatically = false
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
     
     public func checkStatus() {
         let status = CLLocationManager.authorizationStatus()
-        NSLog("status: \(status.rawValue)")
         switch status {
         case .restricted:
+            print("restricted")
             cutoutLocation()
+            showAlert()
             break
             
         case .denied:
+            print("denied")
             cutoutLocation()
+            showAlert()
             break
             
         case .notDetermined:
+            print("notDetermined")
+            
             locationManager.requestWhenInUseAuthorization()
             break
             
         case .authorizedAlways:
+            print("authorizedAlways")
             beginLocation()
             break
             
         case .authorizedWhenInUse:
+            print("authorizedWhenInUse")
             beginLocation()
+            break
+        default:
             break
         }
     }
@@ -47,17 +68,49 @@ public class LocationFramework: NSObject, CLLocationManagerDelegate {
     public func cutoutLocation() {
         
         print("terminate use")
-    
+        
     }
     
     public func beginLocation() {
         
+        setupLocationManager()
+        locationManager.startUpdatingLocation()
+
     }
 
-//-  MARK: Location delegate method
+    //MARK: Location delegate method
     public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        NSLog("Here")
+        print("status is \(status)");
+        checkStatus()
     }
     
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if ((lastLocationUpdate == nil) || (fabs((lastLocationUpdate?.timeIntervalSinceNow)!) > 2)) {
+            let location = locations[0];
+            print(location)
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            let locationString = String(format: "%f,,%f", latitude, longitude)
+            locationInformations?.append(locationString)
+            if let updateLocation = updateLocation, let informations = locationInformations {
+                updateLocation(informations)
+            }
+            lastLocationUpdate = NSDate()
+        }
+        
+        
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("失败")
+    }
+    
+    public func showAlert() {
+        let url = NSURL.fileURL(withPath: UIApplicationOpenSettingsURLString, isDirectory: true)
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: ["":""], completionHandler: nil)
+        }
+    }
 }
 
